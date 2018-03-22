130446f0ecdd22106215d3c6584a63deb1530b9e
John Ky <newhoggy@gmail.com>
2018-03-22 22:07:18 +1100

    Fix warnings

--- src/HaskellWorks/Data/PriorityQueue/FingerTree.hs
+++ src/HaskellWorks/Data/PriorityQueue/FingerTree.hs
@@ -74,9 +74,9 @@ instance Foldable (Entry k) where
 data Prio k v = NoPrio | Prio k v
 
 appendPrio :: Ord k => Prio k v -> Prio k v -> Prio k v
-appendPrio (x            ) (NoPrio       ) = x
-appendPrio (NoPrio       ) (y            ) = y
-appendPrio (x@(Prio kx _)) (y@(Prio ky _)) = if kx <= ky then x else y
+appendPrio x             NoPrio        = x
+appendPrio NoPrio        y             = y
+appendPrio x@(Prio kx _) y@(Prio ky _) = if kx <= ky then x else y
 {-# INLINE appendPrio #-}
 
 instance Ord k => S.Semigroup (Prio k v) where
